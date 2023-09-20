f3b7c98db1bd926e17c3bea2a0e87a00148c0ba3
Matt Schonert <mschon@users.noreply.github.com>
2023-09-20 07:30:58 +0000

    implement AWS provider, create S3 bucket (#12)

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
     random = {
       source = "hashicorp/random"
       version = "3.5.1"
@@ -7,17 +11,30 @@ terraform {
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
+
 provider "random" {
   # Configuration options
 }
 
-
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 32
   special          = false
+  lower            = true
+  upper            = false
 }
 
 output "random_bucket_name" {
   value = random_string.bucket_name.result
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  # Bucket naming rules
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = random_string.bucket_name.result
 }
 
