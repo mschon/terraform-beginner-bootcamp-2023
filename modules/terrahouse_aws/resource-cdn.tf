9816ec62b53705b06c128d4ea86a28697391e5a1
Matt Schonert <mschon@users.noreply.github.com>
2023-09-30 04:18:08 +0000

    invalidate cloudfront cache when content version changes (#38)

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -63,3 +63,19 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
 }
+
+
+resource "terraform_data" "invalidate_cache" {
+  triggers_replace = terraform_data.content_version.output
+
+  provisioner "local-exec" {
+    # https://developer.hashicorp.com/terraform/language/expressions/strings#heredoc-strings
+    command = <<COMMAND
+aws cloudfront create-invalidation \
+--distribution-id ${aws_cloudfront_distribution.s3_distribution.id} \
+--paths "/*"
+    COMMAND
+  }
+}
+
+
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "website_endpoint" {
   description = "S3 static website hosting endpoint"
   value = module.terrahouse_aws.website_endpoint
 }
+
+
+output "cloudfront_url" {
+  description = "CloudFront distribution URL"
+  value = module.terrahouse_aws.cloudfront_url
+}
