13835f5f22fcd5f6052a7dbe5d44142a7a992377
Matt Schonert <mschon@users.noreply.github.com>
2023-10-06 20:09:22 -0400

    implement terraform provider block (#44) (#46)

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "bucket_name" {
-  description = "BUcket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "BUcket name for our static website hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "website_endpoint" {
-  description = "S3 static website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "website_endpoint" {
+#   description = "S3 static website hosting endpoint"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
 
-output "cloudfront_url" {
-  description = "CloudFront distribution URL"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "CloudFront distribution URL"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
